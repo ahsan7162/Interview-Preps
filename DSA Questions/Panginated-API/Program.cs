38c13015d81c1358aa8546a2d1a023fc532e90b5
Muhammad Ahsan <ahsan.imran@astera.com>
2024-01-03 10:27:37 +0500

    Added a program for  pagianted api using HTTP client

---
+++ DSA Questions/Panginated-API/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Panginated_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Main1()).Wait();
+           // Console.ReadLine();
+        }
+        static async void Main1()
+        {
+            Console.WriteLine("Hello, World!");
+            var page = 1;
+            var perPage = 5;
+            var accessToken = "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";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", accessToken);
+            while (true)
+            {
+                try
+                {
+                    var response = client.GetAsync($"https://api.github.com/users/ahsan7162/repos?page={page}&per_page={perPage}").Result;
+                    if (!response.IsSuccessStatusCode)
+                        break;
+                    var data = await response.Content.ReadAsStringAsync();
+                    JArray? obj = JsonConvert.DeserializeObject(data) as JArray;
+                    if (obj is not null && obj.Count == 0)
+                        break;
+                    Console.WriteLine(obj);
+                    page++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
